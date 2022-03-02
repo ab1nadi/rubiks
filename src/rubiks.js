3937e5a69d597a1b5d8bd55b156b7b1b03c73583
abinadi swapp <abinadiswapp@gmail.com>
2022-03-02 15:38:01 -0700

    added some controls

--- src/rubiks.js
+++ src/rubiks.js
@@ -8,10 +8,12 @@ let camera, scene, renderer
 let controls;
 
 
-let groupEverything = new THREE.Group();
-
+let rubiksGroup = new THREE.Group();
+let starGroup = new THREE.Group();
 
 export let k = new Rubiks(0,0,0,3,15);
+
+let pivot = new THREE.Group();
 
 
 export function init(domElementClass, appendedClass) {
@@ -29,43 +31,48 @@ export function init(domElementClass, appendedClass) {
 	scene.add( pointLight );
 
 	// generate random stars
-	for(let i =0; i< 300; i++ )
+	for(let i =0; i< 400; i++ )
 	{
 		let x_ = Math.floor(Math.random() *500)-250;
 		let y_ = Math.floor(Math.random() *500)-250;
 		let z_ = Math.floor(Math.random() *500)-250;
 
-		let geometry = new THREE.BoxGeometry( .5, .5, .5 );
+		let geometry = new THREE.BoxGeometry( .2, .2, .2 );
 		let material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
 		let cube = new THREE.Mesh( geometry, material );
 
 		cube.position.set(x_,y_,z_);
-		scene.add(cube);
+		starGroup.add(cube);
 	}
 
 	
 
-	k.init(groupEverything);
-
-	scene.add(groupEverything)
+	k.init(rubiksGroup);
 
 
 
+
+	pivot.add(rubiksGroup, starGroup)
+
+
+
+	scene.add(pivot)
 
 
 
 	//controls.update() must be called after any manual changes to the camera's transform
-	camera.position.set( 0, 20, 100 );
+	camera.position.set( -20, 0, 40 );
 	
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.screen.width, window.screen.height );
 
-	controls = new OrbitControls( camera, renderer.domElement );
-
 	let d = renderer.domElement;
+
+
+
 	d.classList.add(appendedClass)
-	return document.getElementByClassName(domElementClass)[0].appendChild(d);
+	return document.getElementsByClassName(domElementClass)[0].appendChild(d);
 }
 
 
@@ -74,15 +81,22 @@ export function animate()
 {
 		requestAnimationFrame( animate );
 		k.update();
-		groupEverything.rotation.x +=0.01;
-		groupEverything.rotation.y +=0.01;
-		groupEverything.rotation.z -=0.005;
 
-		controls.update();
+		starGroup.rotation.x+=0.0001;
+		starGroup.rotation.y+=0.0001;
+		starGroup.rotation.z+=0.0002;
 
+		rubiksGroup.rotation.x-=0.001;
+		rubiksGroup.rotation.y+=0.002;
 		renderer.render( scene, camera );
 }
 
+
+export function setCameraPosition(x,y)
+{
+	camera.position.x = x;
+	camera.position.y = y;
+}
 
 
 export function doodle()
