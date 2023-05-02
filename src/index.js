932f4ddbea961810e700a48f4575bcb9cc029da5
ab1nadi <abinadiswapp@gmail.com>
2023-05-01 23:17:26 -0600

    Added query parameters to index.js so that I can tell this rubiks where I want it if I want to use this in an iframe in another project, such as my personal website

--- src/index.js
+++ src/index.js
@@ -1,16 +1,85 @@
-import {init, animate, doodle,rotateCamera} from "./rubiks.js"
+import {init, animate, doodle, setPivotPosition} from "./rubiks.js"
 
 
-init("holder")
+init("holder");
 animate();
 
+const params = new URLSearchParams(window.location.search)
+
+let top = window.innerHeight/2;
+let left = window.innerWidth/2;
+let rightPadding = false;
+let loop = false;
+let animationOffset = 3000;
+
+// get query parameters
+// if they exist
+if(params.has('top'))
+    top = params.get('top');
+
+if(params.has('left'))
+    left = params.get('left');
+
+if(params.has('rightpadding'))
+    rightPadding = params.get('rightpadding');
+
+if(params.has('loop'))
+    loop = params.get('loop');
+
+if(params.has('animationOffset'))
+    animationOffset = parseInt(params.get('animationOffset'));
+
+
+// set the initial position
+setPivotPosition(parseInt(left), parseInt(top));
+
+let currentPosition = parseInt(left);
+
+updatePosition();
+
+// we want the rubiks cube to go left when we resize
+window.addEventListener("resize", (e) =>{
+    updatePosition();
+});
 
 
 
-setTimeout(()=>doodle(),3000);
+runDoodle();
 
 
+// runDoodle
+// essentially this function
+// runs the animation that
+// generates a rubiks puzzle
+// then solves it
+function runDoodle()
+{
+    setTimeout(() => {
+        doodle();
+        if(loop)
+            runDoodle();
+    }, animationOffset);
+}
 
+// updatePosition
+// updates the position of
+// the cube given a right padding
+// if nothing was passed it won't update the position
+function updatePosition()
+{
+    if(rightPadding == false)
+        return;
+
+    if(window.innerWidth <(parseInt(left)+parseInt(rightPadding)))
+       {
+            currentPosition = parseInt(window.innerWidth)-parseInt(rightPadding);
+       } 
+    else if(window.innerWidth> parseInt(left)+parseInt(rightPadding) && currentPosition < parseInt(left))
+       {
+            currentPosition = parseInt(left);
+       }
+    setPivotPosition(currentPosition, parseInt(top));
+}
 
 
 
