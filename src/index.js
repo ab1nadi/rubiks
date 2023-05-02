2f08ca157cc880321b2c231bcfee5f5b2c78717d
abinadi swapp <abinadiswapp@gmail.com>
2023-05-02 08:04:34 -0600

    Moved some code around so that it gets run at the correct time.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ animate();
 
 const params = new URLSearchParams(window.location.search)
 
-let top = window.screen.availHeight/2;
-let left = window.screen.availWidth/2;
+let top = window.screen.height/2;
+let left = window.screen.width/2;
 let rightPadding = false;
 let loop = false;
 let animationOffset = 3000;
 
+let currentPosition = left;
 // get query parameters
 // if they exist
 
@@ -32,15 +33,21 @@ setTimeout(()=>
     if(params.has('animationOffset'))
         animationOffset = parseInt(params.get('animationOffset'));
 
+
+
+    // set the initial position
+    setPivotPosition(parseInt(left), parseInt(top));
+
+    currentPosition = parseInt(left);
+
+    updatePosition();
+
+    runDoodle();
+
 },30);
 
 
-// set the initial position
-setPivotPosition(parseInt(left), parseInt(top));
 
-let currentPosition = parseInt(left);
-
-updatePosition();
 
 // we want the rubiks cube to go left when we resize
 window.addEventListener("resize", (e) =>{
@@ -49,7 +56,7 @@ window.addEventListener("resize", (e) =>{
 
 
 
-runDoodle();
+
 
 
 // runDoodle
@@ -72,9 +79,9 @@ function runDoodle()
 // if nothing was passed it won't update the position
 function updatePosition()
 {
-    if(rightPadding == false)
+    if(!rightPadding)
         return;
-
+        
     if(window.innerWidth <(parseInt(left)+parseInt(rightPadding)))
        {
             currentPosition = parseInt(window.innerWidth)-parseInt(rightPadding);
